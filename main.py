007d4d0fed4f1e19fce542ca51eaada414696ee1
saliorll <lucas.sali--orliange@edu.esiee.fr>
2023-09-27 11:27:12 +0200

    Ajout API_key DataGouv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
 
-print('hello world ! (2)')
+API_DGouv_key = ("eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyIjoiNjUxM2YzOWZlYWE5ZjFlOGZlZjRhMDdjIiwidGltZSI6MTY5NTgwNjUwNi40NDgw"
+                 "NzM0fQ.W9FrAeZlhTToSows3MYUtPGmJufIejFnA_xOrkMQgapbx5XSaWsmI1YGmAqeb2p2xDJ-_Q6ejza1NYkwErCUMQ")
 
