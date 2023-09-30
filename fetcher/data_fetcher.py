71ac510f8f133e1c35a75e9f5e28bb8bf886709f
carangeh <hugo.carangeot@edu.esiee.fr>
2023-09-30 15:33:01 +0200

    seance 1 data_processor.py

--- fetcher/data_fetcher.py
+++ fetcher/data_fetcher.py
@@ -1,11 +1,14 @@
 import requests
+from urllib.parse import urlparse
 
+url = "https://www.data.gouv.fr/fr/datasets/r/64e02cff-9e53-4cb2-adfd-5fcc88b2dc09"
 
-class DataFetcher:
-    def __init__(self, url):
-        self.url = url
+response = requests.get(url)
 
-    def fetch_data(self):
-        response = requests.get(self.url)
-        response.raise_for_status()
-        return response.content
+if response.status_code == 200:
+    file_name = urlparse(url).path.split("/")[-1] + ".csv"  # Ajout de l'extension .csv
+    with open(file_name, 'wb') as file:
+        file.write(response.content)
+        print(f"Le fichier {file_name} a été téléchargé avec succès.")
+else:
+    print("Erreur lors du téléchargement du fichier.")
