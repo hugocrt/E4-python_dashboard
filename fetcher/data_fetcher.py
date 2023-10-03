2f02977c75c2336a419fe422da12a9087c427414
saliorll <lucas.sali--orliange@edu.esiee.fr>
2023-10-03 10:03:05 +0200

    Modification return finale et ajout de la date

--- fetcher/data_fetcher.py
+++ fetcher/data_fetcher.py
@@ -1,67 +1,52 @@
 import requests
 import pandas as pd
-from urllib.parse import urlparse
-
+from datetime import datetime
 
 class DataFetcher:
     def __init__(self, url):
-        """
-        Initialize a DataFetcher object with a given URL.
-
-        :param url: The URL to fetch data from.
-        """
         self.url = url
 
     def fetch_data(self):
-        """
-        Fetch data from the specified URL.
-
-        :return: Content from self.url
-        """
         try:
-            # Use HTTP requests to download data from the URL while handling HTTP errors
             response = requests.get(self.url)
             response.raise_for_status()
-            return response.content
+            content = response.content
+            content_disposition = response.headers.get("content-disposition")
+            return content, content_disposition
         except requests.exceptions.RequestException as e:
             raise Exception(f"Failed to fetch data: {e}")
 
-    def convert_csv(self, fetched_data):
-        """
-        Convert a CSV into a DataFrame using pandas.
-
-        :param fetched_data: Content from self.url
-        :return: DataFrame with content from the CSV file
-        """
+    def import_data(self):
         try:
-            # Define file_name as the ID of the DataSet
-            file_name = "Fuel_price_FR.csv"
+            data, content_disposition = self.fetch_data()
+            file_name = content_disposition.split("filename=")[-1].strip('"')
+            df = self.convert_csv(data, file_name)
 
-            # Write the file_name.csv by opening the fetched_data content, then close it
+            # Obtenez la date actuelle au format de l'OS
+            current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            # Créez un tuple contenant le DataFrame et la date
+            result_tuple = (df, current_date)
+
+            print("File created correctly")
+            return result_tuple
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+    def convert_csv(self, data, file_name):
+        try:
             with open(file_name, 'wb') as file:
-                file.write(fetched_data)
+                file.write(data)
 
-            # Create the DataFrame by separating with ';'
-            df = pd.read_csv(file_name, delimiter=';')
+            df = pd.read_csv(file_name, delimiter=";")
 
             return df
         except Exception as e:
             raise Exception(f"Failed to convert CSV: {e}")
 
+if __name__ == "__main__":
+    fetcher = DataFetcher("https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/exports/csv?lang=fr&timezone=Europe%2FParis&use_labels=true&delimiter=%3B")
+    result_tuple = fetcher.import_data()
 
-url = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/exports/csv?lang=fr&timezone=Europe%2FParis&use_labels=true&delimiter=%3B"
+    print(result_tuple)
 
-# Create an instance of the DataFetcher class, specifying the URL of the file
-fetcher = DataFetcher(url)
-
-try:
-    # Call the fetch_data method to download the data
-    data = fetcher.fetch_data()
-
-    # Then call the convert_csv method to save the data in CSV format and create a DataFrame
-    df = fetcher.convert_csv(data)
-
-    # Now you can work with the DataFrame 'df' as needed
-    print("File created correctly")
-except Exception as e:
-    print(f"An error occurred: {e}")
