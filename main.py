33bfad54ce1f58e30f13b7a63b345d091f9930b3
saliorll <lucas.sali--orliange@edu.esiee.fr>
2023-10-05 12:35:50 +0000

    Ajout du code "main.py" pour la partie data_fetcher (création du fichier csv).

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from processor.data_processor import DataFrameHolder
 
+# Donwload and put the .csv file in the folder "fetcher"
+browser = webdriver.Firefox()
+browser.get('https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2/'
+            'export/')
+browser.implicitly_wait(5)
+url_data_gouv = browser.find_element(By.XPATH, '/html/body/div[1]/main/div/div[4]/div[3]/div[2]/div[7]/div/'
+                                                'div/div/div[1]/ul[1]/li[1]/div/a').get_attribute('href')
+browser.quit()
+
+fetcher = DataFetcher(url_data_gouv)
+result_tuple = fetcher.import_data()
+# Check the value (df, last modification date)
+print(result_tuple)
+
 # On traite nos données de prix des carburants des voitures en france
 df_holder = DataFrameHolder('prix-des-carburants-en-france-flux-instantane-v2.csv')
 df_holder.process_data()
