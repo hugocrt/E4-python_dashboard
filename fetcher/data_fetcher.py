08783810f6ca0ee8eeef6982b23e6b7051f79196
saliorll <lucas.sali--orliange@edu.esiee.fr>
2023-10-05 10:52:25 +0000

    Version fonctionnelle, "__main__" à supprimer une fois le main.py construit

--- fetcher/data_fetcher.py
+++ fetcher/data_fetcher.py
@@ -4,6 +4,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
+def get_coordinates_from_city_names(list_cities):
+    """
+    Get coordinates (latitude and longitude) for a list of city names using OpenStreetMap Nominatim.
+
+    Args:
+        list_cities (list): List of city names.
+
+    Returns:
+        list: List of tuples containing latitude and longitude coordinates.
+    """
+    coordinates_list = []
+    base_url = "https://nominatim.openstreetmap.org/search"
+
+    for city in list_cities:
+        params = {
+            "q": city,
+            "format": "json",
+            "limit": 1
+        }
+
+        try:
+            response = requests.get(base_url, params=params)
+            data = response.json()
+
+            if data:
+                lat = float(data[0]["lat"])
+                lon = float(data[0]["lon"])
+                coordinates_list.append((lat, lon))
+            else:
+                print(f"Unable to find coordinates for {city}.")
+        except Exception as e:
+            print(f"Error while querying {city}: {str(e)}")
+
+    return coordinates_list
 
 class DataFetcher:
     """
@@ -19,42 +53,6 @@ class DataFetcher:
             target_url (str): The URL from which data will be fetched.
         """
         self.target_url = target_url
-
-    @staticmethod
-    def _get_coordinates_from_city_names(list_cities):
-        """
-        Get coordinates (latitude and longitude) for a list of city names using OpenStreetMap Nominatim.
-
-        Args:
-            list_cities (list): List of city names.
-
-        Returns:
-            list: List of tuples containing latitude and longitude coordinates.
-        """
-        coordinates_list = []
-        base_url = "https://nominatim.openstreetmap.org/search"
-
-        for city in list_cities:
-            params = {
-                "q": city,
-                "format": "json",
-                "limit": 1
-            }
-
-            try:
-                response = requests.get(base_url, params=params)
-                data = response.json()
-
-                if data:
-                    lat = float(data[0]["lat"])
-                    lon = float(data[0]["lon"])
-                    coordinates_list.append((lat, lon))
-                else:
-                    print(f"Unable to find coordinates for {city}.")
-            except Exception as e:
-                print(f"Error while querying {city}: {str(e)}")
-
-        return coordinates_list
 
     def fetch_data(self):
         """
@@ -110,25 +108,34 @@ class DataFetcher:
         Returns:
             str: The current date as text.
         """
+        options = webdriver.FirefoxOptions()
+        options.headless = True
         try:
-            options = Options()
-            options.headless = True
-            browser = webdriver.Firefox()
-            browser.get("https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2/information/")
-            date_element = browser.find_element(by=By.XPATH, value="/html/body/div[1]/main/div/div[4]/div[2]/div[2]/div[1]/div/div[2]/div/div[3]/div[6]/div[2]")
+            browser = webdriver.Firefox(options=options)
+            browser.get("https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2"
+                        "/information/")
+            # Need to wait to be sure that the web page is fully opened
+            browser.implicitly_wait(5)
+            # Get through XPATH the "content" in the tag at the XPATH
+            date_element = browser.find_element(by=By.XPATH, value="/html/body/div[1]/main/div/div[4]/div[3]/div[2]/"
+                                                                   "div[1]/div/div[2]/div/div[3]/div[6]/div[2]")
             date_text = date_element.text
             browser.quit()
             return date_text
         except Exception as e:
-            raise Exception(f"Failed to get current date: {e}")
-
+            raise Exception(f"Failed to get the current date: {e}")
 
 if __name__ == "__main__":
     browser = webdriver.Firefox()
-    browser.get('https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2/export/')
-    url_data_gouv = browser.find_element(by=By.XPATH, value="/html/body/div[1]/main/div/div[4]/div[2]/div[2]/div[7]/div/div/div/div[1]/ul[1]/li[1]/div/a").get_attribute('href')
+    browser.get('https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2/'
+                'export/')
+    # Need to wait to be sure that the web page is fully opened
+    browser.implicitly_wait(5)
+    # Get through XPATH the "href" from the tag at the XPATH
+    url_data_gouv = browser.find_element(By.XPATH, '/html/body/div[1]/main/div/div[4]/div[3]/div[2]/div[7]/div/'
+                                                   'div/div/div[1]/ul[1]/li[1]/div/a').get_attribute('href')
     browser.quit()
+
     fetcher = DataFetcher(url_data_gouv)
     result_tuple = fetcher.import_data()
     print(result_tuple)
-    print()
