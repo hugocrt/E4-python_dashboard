9f3a63227b1086e45dabb7b0f799e99dca8a81bb
carangeh <hugo.carangeot@edu.esiee.fr>
2023-10-06 13:47:24 +0000

    data_processor.py v3, 10/10 pylint

--- processor/data_processor.py
+++ processor/data_processor.py
@@ -1,4 +1,4 @@
-# Import some librairies
+"""Module providing methods on our specific dataframe"""
 from tkinter import messagebox
 from pathlib import Path
 import pandas as pd
@@ -10,7 +10,7 @@ class DataFrameHolder:
 
     :param:
         current_dir (Path): The current directory path.
-        df (pd.DataFrame): The DataFrame to be processed.
+        data_frame (pd.DataFrame): The DataFrame to be processed.
         price_columns (list): List of column names containing price information.
     """
 
@@ -24,9 +24,10 @@ class DataFrameHolder:
         # Set the current file directory for opening and saving file
         self.current_dir = Path(__file__).resolve().parent
         # Set a dataframe for further processes
-        self.df = self.load_csv_file(file_name)
+        self.data_frame = self.load_csv_file(file_name)
         # Set the fuels
-        self.price_columns = ['Gazole_prix', 'SP98_prix', 'SP95_prix', 'E85_prix', 'E10_prix', 'GPLc_prix']
+        self.price_columns = ['Gazole_prix', 'SP98_prix', 'SP95_prix',
+                              'E85_prix', 'E10_prix', 'GPLc_prix']
 
     def load_csv_file(self, file_name):
         """
@@ -44,14 +45,14 @@ class DataFrameHolder:
         # Errors handling, we try to open the file, if we got an error print it in a box (with tk)
         try:
             return pd.read_csv(csv_path, dtype={'Code postal': 'object'}, delimiter=';')
-        except FileNotFoundError:
-            messagebox.showerror("Error", f"The file '{csv_path}' was not found.")
+        except FileNotFoundError as exception:
+            messagebox.showerror("Error", f"The file '{csv_path}' was not found: {exception}")
             return None
-        except pd.errors.EmptyDataError:
-            messagebox.showerror("Error", f"The file '{csv_path}' is empty.")
+        except pd.errors.EmptyDataError as exception:
+            messagebox.showerror("Error", f"The file '{csv_path}' is empty: {exception}")
             return None
-        except Exception as e:
-            messagebox.showerror("Error", f"An error occurred while opening the file: {e}")
+        except Exception as exception:  # pylint: disable=broad-except
+            messagebox.showerror("Error", f"An error occurred: {exception}")
             return None
 
     def process_data(self):
@@ -67,40 +68,48 @@ class DataFrameHolder:
         """
         Performs data cleaning operations on the DataFrame.
         """
-        useful_columns = ['Région', 'Département', 'Code postal', 'Ville', 'geom'] + self.price_columns
+        useful_columns = (['Région', 'Département', 'Code postal', 'Ville', 'geom']
+                          + self.price_columns)
 
         # We do not need the others columns for our application
-        self.df = self.df[useful_columns]
-        # We need unique key to perform grouping (ville is not sufficient, can have same names in different area)
-        self.df['cp_ville'] = self.df['Code postal'] + ' ' + self.df['Ville']
+        self.data_frame = self.data_frame[useful_columns]
+        # We need unique key to perform grouping (ville is not sufficient, can have same names in
+        # different area)
+        self.data_frame['cp_ville'] = (self.data_frame['Code postal'] + ' '
+                                       + self.data_frame['Ville'])
         # Then we do not have use of these
-        self.df = self.df.drop(columns=['Ville', 'Code postal'])
+        self.data_frame = self.data_frame.drop(columns=['Ville', 'Code postal'])
         # There are data without specify (Région, département, Ville) so we delete these
-        self.df = self.df.dropna(subset=['Région'])
+        self.data_frame = self.data_frame.dropna(subset=['Région'])
         # Split geom and floating them to have latitude and longitude for further application
-        self.df['geom'] = self.df['geom'].apply(lambda x: [float(coord) for coord in x.split(', ') if coord])
+        self.data_frame['geom'] = (self.data_frame['geom']
+                                   .apply(lambda x:
+                                   [float(coord) for coord in x.split(', ') if coord]))
 
     def _compute_new_dataframe(self):
         """
         Computes a new DataFrame by performing various operations.
         """
         # Mapping to have region and department linked to each city
-        city_geo_mapping = self.df.groupby('cp_ville')[['Région', 'Département']].first().reset_index()
+        city_geo_mapping = (self.data_frame.groupby('cp_ville')[['Région', 'Département']]
+                            .first().reset_index())
 
         # Performing the average prices for each city
-        city_prices_means = self.df.groupby('cp_ville')[self.price_columns].mean().reset_index()
+        city_prices_means = (self.data_frame.groupby('cp_ville')[self.price_columns]
+                             .mean().reset_index())
 
-        # Performing the average coordinates for each city
-        city_coords_means = pd.Series((self.df.groupby('cp_ville')['geom'])
-                                      .apply(lambda x: self._mean_coords(x)), name='geom')
+        city_coords_means = (self.data_frame.groupby('cp_ville')['geom']
+                             .apply(self._mean_coords)
+                             .reset_index(level=1, drop=True)
+                             .reset_index())
 
         # Count how many times a city appears
-        city_app_count = self.df.groupby('cp_ville').size().reset_index(name='Apparition')
+        city_app_count = self.data_frame.groupby('cp_ville').size().reset_index(name='Apparition')
 
         # merge all results to have one dataframe
-        self.df = (city_geo_mapping.merge(city_prices_means, on='cp_ville')
-                                   .merge(city_coords_means, on='cp_ville')
-                                   .merge(city_app_count, on='cp_ville'))
+        self.data_frame = (city_geo_mapping.merge(city_prices_means, on='cp_ville')
+                                           .merge(city_coords_means, on='cp_ville')
+                                           .merge(city_app_count, on='cp_ville'))
 
     @staticmethod
     def _mean_coords(coords_list):
@@ -117,7 +126,8 @@ class DataFrameHolder:
         latitudes = [coord[0] for coord in coords_list if coord]
         longitudes = [coord[1] for coord in coords_list if coord]
         # return coordinates tuple
-        return sum(latitudes) / len(latitudes), sum(longitudes) / len(longitudes)
+        return pd.DataFrame({'Latitude': [sum(latitudes) / len(latitudes)],
+                             'Longitude': [sum(longitudes) / len(longitudes)]})
 
     def save_processed_dataframe(self):
         """
@@ -132,6 +142,6 @@ class DataFrameHolder:
         # Errors handling, we try to save the file, if we got an error print it in a box (with tk)
         try:
             file_path = target_dir / 'processed_data.csv'
-            self.df.to_csv(file_path, index=False)
-        except Exception as e:
-            messagebox.showerror("Error", f"An error occurred: {e}")
+            self.data_frame.to_csv(file_path, index=False)
+        except Exception as exception:  # pylint: disable=broad-except
+            messagebox.showerror("Error", f"An error occurred: {exception}")
