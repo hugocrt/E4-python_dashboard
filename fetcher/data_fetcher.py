d2f5ef843381f34c5a1cb59d65ec517eb784d46b
saliorll <lucas.sali--orliange@edu.esiee.fr>
2023-10-06 12:25:01 +0000

    Update data_fetcher.py

--- fetcher/data_fetcher.py
+++ fetcher/data_fetcher.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 import pandas as pd
 import tkinter as tk
@@ -6,147 +8,127 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def get_coordinates_from_city_names(list_cities):
-    """
-    Get coordinates (latitude and longitude) for a list of city names using OpenStreetMap Nominatim.
-
-    Args:
-        list_cities (list): List of city names.
-
-    Returns:
-        list: List of tuples containing latitude and longitude coordinates.
-    """
-    coordinates_list = []
-    base_url = "https://nominatim.openstreetmap.org/search"
-
-    for city in list_cities:
-        params = {
-            "q": city,
-            "format": "json",
-            "limit": 1
-        }
-
-        try:
-            response = requests.get(base_url, params=params)
-            data = response.json()
-
-            if data:
-                lat = float(data[0]["lat"])
-                lon = float(data[0]["lon"])
-                coordinates_list.append((lat, lon))
-            else:
-                print(f"Unable to find coordinates for {city}.")
-        except Exception as e:
-            print(f"Error while querying {city}: {str(e)}")
-
-    return coordinates_list
-
-
 def show_error_message(message):
     root = tk.Tk()
-    root.withdraw()  # Hide the main window
+    root.withdraw()
     messagebox.showerror("Error", message)
     root.destroy()
+
+
+def get_data_from_xpath(url, xpath, content="href"):
+    """
+        Scrapes data from a webpage using Selenium.
+
+        Args:
+            url (str): The URL of the webpage to scrape.
+            xpath (str): The XPath expression to locate the element on the webpage.
+            content (str, optional): The type of content to retrieve ("href" or "text"). Default is "href".
+
+        Returns:
+            str: The scraped data based on the specified content type (.csv or date)
+
+        Raises:
+            Exception: If there's an error during scraping.
+    """
+    options = webdriver.FirefoxOptions()
+    options.headless = True
+
+    try:
+        with webdriver.Firefox(options=options) as driver:
+            driver.get(url)
+            # Wait to load the whole page
+            driver.implicitly_wait(5)
+
+            # Get the element based on the provided XPATH
+            element = driver.find_element(By.XPATH, xpath)
+
+            if content == "href":
+                data_from_url = element.get_attribute("href")
+            elif content == "text":
+                data_from_url = element.text
+            else:
+                raise ValueError("Invalid content type")
+
+        return data_from_url
+
+    except Exception as e:
+        error_message = f"Failed to get data from the webpage: {e}"
+        show_error_message(error_message)
+        raise Exception(error_message)
 
 
 class DataFetcher:
     """
     This class allows fetching data from a specified URL, saving it to a CSV file,
-    importing it into a Pandas DataFrame, and obtaining the current date from a web page.
+    importing it into a Pandas DataFrame, and obtaining the current date from a web page with the last date of
+    modification.
     """
 
-    def __init__(self, target_url):
+    def __init__(self,
+                 base_url="https://data.economie.gouv.fr/explore/dataset/"
+                          "prix-des-carburants-en-france-flux-instantane-v2/",
+                 file_name="prix-des-carburants-en-france-flux-instantane-v2.csv"):
         """
-        Initialize a DataFetcher object with a specified URL.
+                Initialize a DataFetcher object with a specified URL.
 
-        Args:
-            target_url (str): The URL from which data will be fetched.
-        """
-        self.target_url = target_url
+                Args:
+                    base_url (str): The root URL from which data & date will be fetched.
+                """
+        self.download_url = base_url + "export/"
+        self.date_url = base_url + "information/"
+        self.file_name = file_name
+        self.data = None
+        self.date = None
 
-    def fetch_data(self):
-        """
-        Fetch data from the specified URL.
+    def get_csv_url(self):
+        return get_data_from_xpath(self.download_url, "/html/body/div[1]/main/div/div[4]/div[3]/div[2]/div[7]/"
+                                                      "div/div/div/div[1]/ul[1]/li[1]/div/a", content="href")
 
-        Returns:
-            tuple: A tuple containing the raw data and the 'content-disposition' header value.
-        """
+    def get_date(self):
+        self.date = get_data_from_xpath(self.date_url, xpath="/html/body/div[1]/main/div/div[4]/div[3]/div[2]/div[1]/"
+                                                             "div/div[2]/div/div[3]/div[6]/div[2]", content="text")
+
+    def save_file_as_csv(self):
         try:
-            response = requests.get(self.target_url)
+            csv_url = self.get_csv_url()
+            file_name = self.file_name
+
+            response = requests.get(csv_url)
             response.raise_for_status()
-            content = response.content
-            content_disposition = response.headers.get("content-disposition")
-            return content, content_disposition
+
+            local_path = os.path.join(os.getcwd(), file_name)
+
+            with open(local_path, 'wb') as file:
+                file.write(response.content)
+
+            print(f"Le fichier CSV '{file_name}' a été téléchargé avec succès.")
         except requests.exceptions.RequestException as e:
-            show_error_message(f"Failed to fetch data: {e}")
+            show_error_message(f"Erreur lors de la demande HTTP : {e}")
+        except IOError as e:
+            show_error_message(f"Erreur lors de l'écriture du fichier local : {e}")
 
-    def import_data(self):
-        """
-        Import data into a Pandas DataFrame from the specified URL.
 
-        Returns:
-            tuple: A tuple containing the Pandas DataFrame of data and the current date.
-        """
-        try:
-            data, content_disposition = self.fetch_data()
-            file_name = content_disposition.split("filename=")[-1].strip('"')
-            self.save_data_to_file(data, file_name)
-            df = pd.read_csv(file_name, delimiter=";")
-            date_text = self.get_current_date()
-            return df, date_text
-        except Exception as e:
-            show_error_message(f"An error occurred: {e}")
+def main():
+    # Créez une instance de la classe DataFetcher avec l'URL de base appropriée
+    data_fetcher = DataFetcher(base_url="https://data.economie.gouv.fr/explore/dataset/"
+                                        "prix-des-carburants-en-france-flux-instantane-v2/")
 
-    def save_data_to_file(self, data, file_name):
-        """
-        Save raw data to a file.
+    try:
+        # Obtenez l'URL du fichier CSV
+        csv_url = data_fetcher.get_csv_url()
 
-        Args:
-            data (bytes): The raw data to be saved.
-            file_name (str): The name of the file where the data will be saved.
-        """
-        try:
-            with open(file_name, 'wb') as file:
-                file.write(data)
-        except Exception as e:
-            show_error_message(f"Failed to save data to file: {e}")
+        # Téléchargez et sauvegardez le fichier CSV localement
+        data_fetcher.save_file_as_csv()
 
-    def get_current_date(self):
-        """
-        Get the current date from a web page.
+        # Obtenez la date de la page web
+        data_fetcher.get_date()
 
-        Returns:
-            str: The current date as text.
-        """
-        options = webdriver.FirefoxOptions()
-        options.headless = True
-        try:
-            driver = webdriver.Firefox(options=options)
-            driver.get("https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2"
-                       "/information/")
-            # Need to wait to be sure that the web page is fully opened
-            driver.implicitly_wait(5)
-            # Get through XPATH the "content" in the tag at the XPATH
-            date_element = driver.find_element(by=By.XPATH, value="/html/body/div[1]/main/div/div[4]/div[3]/div[2]/"
-                                                                  "div[1]/div/div[2]/div/div[3]/div[6]/div[2]/span")
-            date_text = date_element.text
-            driver.quit()
-            return date_text
-        except Exception as e:
-            show_error_message(f"Failed to get the current date: {e}")
+        # Affichez les données et la date obtenues
+        print("Données CSV téléchargées :", csv_url)
+        print("Date de dernière modification :", data_fetcher.date)
+    except Exception as e:
+        print(f"Une erreur s'est produite : {e}")
 
 
 if __name__ == "__main__":
-    browser = webdriver.Firefox()
-    browser.get('https://data.economie.gouv.fr/explore/dataset/prix-des-carburants-en-france-flux-instantane-v2/'
-                'export/')
-    # Need to wait to be sure that the web page
-    browser.implicitly_wait(5)
-    # Get through XPATH the "href" from the tag at the XPATH
-    url_data_gouv = browser.find_element(By.XPATH, '/html/body/div[1]/main/div/div[4]/div[3]/div[2]/div[7]/div/'
-                                                   'div/div/div[1]/ul[1]/li[1]/div/a').get_attribute('href')
-    browser.quit()
-
-    fetcher = DataFetcher(url_data_gouv)
-    result_tuple = fetcher.import_data()
-    print(result_tuple)
+    main()
